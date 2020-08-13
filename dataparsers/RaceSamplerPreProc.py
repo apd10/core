25eff264a4f3bfa67ebedb18839ed8a2f054a148
Aditya Pradip Desai <apd10@yogi.cs.rice.edu>
2020-08-13 18:42:22 -0500

    letter, kde_lb : Dont Use but a structure for adding modification to sampling . like rejection after sampling

--- dataparsers/RaceSamplerPreProc.py
+++ dataparsers/RaceSamplerPreProc.py
@@ -42,32 +42,61 @@ def sample_m1(rep, label, bucket):
     sample = hitandrun.get_samples(n_samples=1, thin=100)
     return sample[0],label
 
-def sample_batch(num):
+def sample_batch(num, kde_lb):
     race = global_vars["race"]
+    class_counts = global_vars["class_prob"] * num
+    class_counts = class_counts.astype(int) + 1
     class_prob = global_vars["class_prob"]
-    data = []
-    labels = []
-    with concurrent.futures.ProcessPoolExecutor(25) as executor:
-        futures = []
-        print("submitting jobs")
+    num_samples = 0
+    Datas = []
+    Labels = []
 
-        for i in tqdm(range(num)):
-            label = np.argmax(np.random.multinomial(1, class_prob, size=None))
-            rep = np.random.randint(0, race.repetitions)
-            sketch = race.sketch_memory[label] # 1 x range  array of counts
-
-            sketch_data = sketch.data[sketch.indptr[rep]:sketch.indptr[rep+1]]
-            sketch_cols = sketch.indices[sketch.indptr[rep]:sketch.indptr[rep+1]]
-            bucket_idx = np.argmax(np.random.multinomial(1, sketch_data/np.sum(sketch_data), size=None))
-            bucket = sketch_cols[bucket_idx]
-            futures.append(executor.submit(sample_m1, rep, label, bucket))
-        print("waiting for executions")
-        for res in tqdm(concurrent.futures.as_completed(futures), total=num):
-          d = res.result()
-          if d is not None:
-              data.append(d[0])
-              labels.append(d[1])
-    return np.array(data),np.array(labels)
+    while np.sum(class_counts) > 0 and num_samples < num:
+        print("Samples", num_samples, "/", num)
+        print(class_counts)
+        print(class_prob)
+        data = []
+        labels = []
+        with concurrent.futures.ProcessPoolExecutor(40) as executor:
+            futures = []
+            print("submitting jobs")
+            this_num = min((num-num_samples)*10, num)
+    
+            for i in tqdm(range(this_num)):
+                label = np.argmax(np.random.multinomial(1, class_prob, size=None))
+                rep = np.random.randint(0, race.repetitions)
+                sketch = race.sketch_memory[label] # 1 x range  array of counts
+    
+                sketch_data = sketch.data[sketch.indptr[rep]:sketch.indptr[rep+1]]
+                sketch_cols = sketch.indices[sketch.indptr[rep]:sketch.indptr[rep+1]]
+                bucket_idx = np.argmax(np.random.multinomial(1, sketch_data/np.sum(sketch_data), size=None))
+                bucket = sketch_cols[bucket_idx]
+                futures.append(executor.submit(sample_m1, rep, label, bucket))
+            print("waiting for executions")
+            for res in tqdm(concurrent.futures.as_completed(futures), total=this_num):
+              d = res.result()
+              if d is not None:
+                  data.append(d[0])
+                  labels.append(d[1])
+        data =  np.array(data)
+        labels = np.array(labels)
+        if kde_lb is not None:
+          kde_estimates = race.query(data, labels)
+          data = data[kde_estimates > kde_lb]
+          labels = labels[kde_estimates > kde_lb]
+        num_samples += len(labels)
+        # update class prob
+        ls, cts = np.unique(labels, return_counts=True)
+        for li in range(len(ls)):
+          class_counts[ls[li]] = max(0, class_counts[ls[li]] - cts[li])
+        class_prob = class_counts / np.sum(class_counts)
+            
+        Datas.append(data)
+        Labels.append(labels)
+    Data = np.concatenate(Datas, axis=0)
+    Label = np.concatenate(Labels, axis=0)
+    return Data, Label
+        
 
 
 class RaceSamplerPreProc(data.Dataset):
@@ -79,6 +108,9 @@ class RaceSamplerPreProc(data.Dataset):
         self.length = params["epoch_samples"]
         self.parallel_batch = params["parallel_batch"]
         self.method = params["method"]
+        self.kde_lb = None
+        if "kde_lb" in params:
+            self.kde_lb = params["kde_lb"]
         self.race = Race(self.race_pickle["params"])
         self.race.sketch_memory = self.race_pickle["memory"]
         self.race.hashfunction.set_dictionary(self.race_pickle["hashfunction"])
@@ -103,7 +135,7 @@ class RaceSamplerPreProc(data.Dataset):
 
     def __getitem__(self, index):
         if self.Data is None or index + 1 > self.Data.shape[0]:
-            data,labels = sample_batch(self.parallel_batch)
+            data,labels = sample_batch(self.parallel_batch, self.kde_lb)
             if self.Data is None:
                 self.Data = data
                 self.labels = labels
